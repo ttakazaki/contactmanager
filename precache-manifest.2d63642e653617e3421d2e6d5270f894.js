ea4b0cd2828a668e7d002fd2a7013e4d5bb3ca00
Toshifumi Takazaki <urd59675@gmail.com>
2018-10-19 16:13:02 +0900

    Updates

---
+++ precache-manifest.2d63642e653617e3421d2e6d5270f894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "43b8a07796baa8391c80",
+    "url": "/contactmanager/static/js/main.43b8a077.chunk.js"
+  },
+  {
+    "revision": "a44fc581d3826420b123",
+    "url": "/contactmanager/static/js/1.a44fc581.chunk.js"
+  },
+  {
+    "revision": "43b8a07796baa8391c80",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "a44fc581d3826420b123",
+    "url": "/contactmanager/static/css/1.c4c1cb51.chunk.css"
+  },
+  {
+    "revision": "454f134e40c6b98dc500bcb8acbe405a",
+    "url": "/contactmanager/index.html"
+  }
+];
